acdba2a2f4844c3822d6692ba75adefbf1ab71c9
github username <github email address>
2019-05-22 11:19:37 -0700

    serialized team model for json data, image attr added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_190847) do
+ActiveRecord::Schema.define(version: 2019_05_22_181435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2019_05_20_190847) do
     t.string "city"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   add_foreign_key "gameschedules", "games"
