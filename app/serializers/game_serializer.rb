45ed7af7b6687ef01854b018b3a9b4856fad6f92
github username <github email address>
2019-05-22 12:07:59 -0700

    games serializer adds team image

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -2,7 +2,7 @@ class GameSerializer < ActiveModel::Serializer
   attributes :id, :courtnum, :team1score, :team2score, :teams
   def teams
     self.object.teams.map do |team|
-      {name: team.name, city: team.city}
+      {name: team.name, city: team.city, image: team.image}
     end
   end
 end
