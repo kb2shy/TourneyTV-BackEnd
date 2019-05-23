e332629abebe1cac0b7bcb2484ebf0355d08ab4a
github username <github email address>
2019-05-23 16:01:02 -0700

    players serialized

---
+++ app/controllers/players_controller.rb
@@ -0,0 +1,41 @@
+class PlayersController < ApplicationController
+  before_action :set_player, only: [:show, :edit, :update]
+
+  def index
+    @players = Player.all
+    if @players
+      render :json => @players, response: :ok
+    else
+      render :json => {error: "Players not found"}, response: :not_found
+    end
+  end
+
+  def show
+    if @player.valid?
+      render :json => @player, response: :ok
+    else
+      render :json => {error: "Player not found"}, response: :not_found
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @game.update(player_params)
+      redirect_to @player
+    else
+      render :json => {error: "Cannot update player"}, response: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_player
+    @player = Player.find(params[:id])
+  end
+
+  def player_params
+    params.require(:player).permit(:username, :password, :firstname, :lastname, :image, :position, :jersey, :team_id)
+  end
+end
