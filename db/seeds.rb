1df715a1e5528f976498160b78c71f733f264d9a
github username <github email address>
2019-05-23 18:52:37 -0700

    seeds.rb updated to get dc comics pics-already seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,41 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-require 'faker'
+# require 'faker'
+#
+# players = Player.all
+# players.each do |player|
+#   name = Faker::DcComics.unique.name.split(" ")
+#   player.update(
+#     firstname: name[0],
+#     lastname: name[1],
+#     image: '',
+#     position: '',
+#     jersey: 0,
+#    )
+#  end
 
-players = Player.all
-players.each do |player|
-  name = Faker::DcComics.unique.name.split(" ")
-  player.update(
-    firstname: name[0],
-    lastname: name[1],
-    image: '',
-    position: '',
-    jersey: 0,
-   )
- end
+# Player.find(3).update(image: "https://www.dccomics.com/sites/default/files/styles/character_thumb_160x160/public/Char_Profile_Batman_20190116_5c3fc4b40faec2.47318964.jpg?itok=3RkYenfT")
+# Player.find(4).update(image: "https://www.dccomics.com/sites/default/files/styles/character_thumb_160x160/public/Char_Profile_Penguin_5c5250a8ad2683.23365592.jpg?itok=FJMNf6yw")
+# Player.find(5).update(image: "https://www.dccomics.com/sites/default/files/styles/character_thumb_160x160/public/Char_Profile_BlueBeetle_5c4118a7147562.76105620.jpg?itok=rYiA1jxL")
+# Player.find(6).update(image: "https://www.dccomics.com/sites/default/files/styles/character_thumb_160x160/public/Char_Profile_GreenLantern_20190116_5c3fc8c14ceda8.50076512.jpg?itok=WPW1sVms")
+# Player.find(7).update(image: "https://www.dccomics.com/sites/default/files/styles/character_thumb_160x160/public/Char_Profile_WonderWoman_20190116_5c3fc6aa51d0e3.49076914.jpg?itok=YbSnjIc1")
+# Player.find(8).update(image: "https://www.dccomics.com/sites/default/files/styles/character_thumb_160x160/public/Char_Profile_Flash_20190116_5c3fcaaa18f0e8.03668117.jpg?itok=miItS6xr")
+# Player.find(9).update(image: "")
+
+
+# Player.find(10).update(image: "https://www.dccomics.com/sites/default/files/styles/character_thumb_160x160/public/Char_Profile_Penguin_5c5250a8ad2683.23365592.jpg?itok=FJMNf6yw")
+# Player.find(11).update(image: "https://www.dccomics.com/sites/default/files/styles/character_thumb_160x160/public/Char_Profile_Penguin_5c5250a8ad2683.23365592.jpg?itok=FJMNf6yw")
+# Player.find(12).update(image: "https://www.dccomics.com/sites/default/files/styles/character_thumb_160x160/public/Char_Profile_Penguin_5c5250a8ad2683.23365592.jpg?itok=FJMNf6yw")
+# Player.find(13).update(image: "https://www.dccomics.com/sites/default/files/styles/character_thumb_160x160/public/Char_Profile_Penguin_5c5250a8ad2683.23365592.jpg?itok=FJMNf6yw")
+# Player.find(14).update(image: "https://www.dccomics.com/sites/default/files/styles/character_thumb_160x160/public/Char_Profile_Penguin_5c5250a8ad2683.23365592.jpg?itok=FJMNf6yw")
+# Player.find(15).update(image: "https://www.dccomics.com/sites/default/files/styles/character_thumb_160x160/public/Char_Profile_Penguin_5c5250a8ad2683.23365592.jpg?itok=FJMNf6yw")
+# Player.find(16).update(image: "https://www.dccomics.com/sites/default/files/styles/character_thumb_160x160/public/Char_Profile_Penguin_5c5250a8ad2683.23365592.jpg?itok=FJMNf6yw")
+# Player.find(17).update(image: "https://www.dccomics.com/sites/default/files/styles/character_thumb_160x160/public/Char_Profile_Penguin_5c5250a8ad2683.23365592.jpg?itok=FJMNf6yw")
+# Player.find(18).update(image: "https://www.dccomics.com/sites/default/files/styles/character_thumb_160x160/public/Char_Profile_Penguin_5c5250a8ad2683.23365592.jpg?itok=FJMNf6yw")
+# Player.find(19).update(image: "https://www.dccomics.com/sites/default/files/styles/character_thumb_160x160/public/Char_Profile_Penguin_5c5250a8ad2683.23365592.jpg?itok=FJMNf6yw")
+# Player.find(20).update(image: "https://www.dccomics.com/sites/default/files/styles/character_thumb_160x160/public/Char_Profile_Penguin_5c5250a8ad2683.23365592.jpg?itok=FJMNf6yw")
+# Player.find(21).update(image: "https://www.dccomics.com/sites/default/files/styles/character_thumb_160x160/public/Char_Profile_Penguin_5c5250a8ad2683.23365592.jpg?itok=FJMNf6yw")
+# Player.find(22).update(image: "https://www.dccomics.com/sites/default/files/styles/character_thumb_160x160/public/Char_Profile_Penguin_5c5250a8ad2683.23365592.jpg?itok=FJMNf6yw")
+# Player.find(23).update(image: "https://www.dccomics.com/sites/default/files/styles/character_thumb_160x160/public/Char_Profile_Penguin_5c5250a8ad2683.23365592.jpg?itok=FJMNf6yw")
+# Player.find(24).update(image: "https://www.dccomics.com/sites/default/files/styles/character_thumb_160x160/public/Char_Profile_Penguin_5c5250a8ad2683.23365592.jpg?itok=FJMNf6yw")
