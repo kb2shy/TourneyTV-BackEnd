2a3c9475e868c76a75531257308a4fb4b826b3eb
github username <github email address>
2019-05-28 16:23:20 -0700

    added isScoreKeeper and isTeamCaptain to player serializer

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -1,3 +1,4 @@
 class PlayerSerializer < ActiveModel::Serializer
-  attributes :id, :firstname, :lastname, :image, :position, :jersey, :team
+  attributes :id, :firstname, :lastname, :image, :position, :jersey, :team,
+  :isScoreKeeper, :isTeamCaptain 
 end
